c83021f068f9bb8761fc79000077e87f1c44a92f
Andrey Lygun <lygun@yandex.ru>
2022-07-14 12:20:28 +0300

    Добавлена Яндекс-Карта

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.app')
 
 @section('scripts')
-    <script src="/js/jquery-3.6.0.min.js"></script>
-    <script src="/lightbox/js/lightbox.js"></script>
-    <link href="/lightbox/css/lightbox.css" rel="stylesheet">
+    <script src="/lib/jquery/jquery-3.6.0.min.js"></script>
+    <script src="/lib/lightbox/lightbox.js"></script>
+    <link href="/lib/lightbox/lightbox.css" rel="stylesheet">
+    <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=d5251b8b-7907-42ff-bbf1-284fde59a90a" type="text/javascript"></script>
+    <script src="/lib/yandex/maps.js" type="text/javascript"></script>
+    <style type="text/css">
+        #map {
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 @endsection
 
 
@@ -17,10 +27,15 @@
         </div>
         <div class="row">
             @foreach($Article->photos as $Photo)
-                <a href="/photos/{{$Photo->file}}" class="col-4 col-md-1" data-lightbox="images" >
+                <a href="/photos/{{$Photo->file}}" class="col-3 col-md-1 my-2" data-lightbox="images" >
                     <img src="/photos/{{$Photo->file}}" class="img-fluid" >
                 </a>
             @endforeach
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-8 col-lg-6">
+                <div id="map" style="height: 500px"></div>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,18 +10,10 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-
-    <script src="{{ asset('js/index.bundle.min.js') }}"></script>
+    <script src="/lib/bootstrap/js/bootstrap.bundle.js"></script>
+    <link href="/lib/bootstrap/css/bootstrap.css" rel="stylesheet">
     @yield('scripts')
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
