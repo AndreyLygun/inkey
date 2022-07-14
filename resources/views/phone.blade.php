1e2d8e3a41ea9b038d337a027d0fe5a775cc8eef
Andrey Lygun <lygun@yandex.ru>
2022-07-14 16:14:08 +0300

    Alfa 0.2

--- resources/views/phone.blade.php
+++ resources/views/phone.blade.php
@@ -3,7 +3,12 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @foreach($Articles as $Article)
+            <div class="col">
+                <p>По номеру {{$Articles[0]->phone}} мы нашли {{$Articles->count()}} объявления</p>
+            </div>
+        </div>
+        @foreach($Articles as $Article)
+            <div class="row">
                 <div class="col-8 col-md-10">
                     <p class="h3">{{$Article->title}}</p>
                     <p><a href="/articles/{{$Article->id}}">Подробней</a></p>
@@ -11,7 +16,8 @@
                 <div class="col-4 col-md-2">
                     <img src="/photos/{{$Article->photo}}" class="img-fluid"/>
                 </div>
-            @endforeach
-        </div>
+            </div>
+            <hr>
+        @endforeach
     </div>
 @endsection
