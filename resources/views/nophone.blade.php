627c6400c43721449fedcbcfcfca74a21a1df84e
Andrey Lygun <lygun@yandex.ru>
2022-07-14 16:30:57 +0300

    Обработка запроса без объявлений

---
+++ resources/views/nophone.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p>По номеру {{$Articles[0]->phone}} мы нашли {{$Articles->count()}} объявления</p>
+            </div>
+        </div>
+        @foreach($Articles as $Article)
+            <div class="row">
+                <div class="col-8 col-md-10">
+                    <p class="h3">{{$Article->title}}</p>
+                    <p><a href="/articles/{{$Article->id}}">Подробней</a></p>
+                </div>
+                <div class="col-4 col-md-2">
+                    <img src="/photos/{{$Article->photo}}" class="img-fluid"/>
+                </div>
+            </div>
+            <hr>
+        @endforeach
+    </div>
+@endsection
